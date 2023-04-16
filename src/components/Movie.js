0bff38f356a08a0fa08856783af195ed449f2396
rafa <ggootye@gmail.com>
2023-04-16 22:48:43 +0900

    the first commit

---
+++ src/components/Movie.js
@@ -0,0 +1,31 @@
+import PropTypes from "prop-types";
+import {Link} from "react-router-dom";
+import React from "react";
+
+
+function Movie({id, coverImg, title, summary, genres}){
+    return (
+
+    <div>
+    <img src={coverImg} alt='hello'/>
+      <h2><Link to={`/movie/${id}`}>{title}</Link></h2>
+      <p>{summary.length > 235 ? `${summary.slice(0,235)}...` : summary}</p>
+      <ul>
+        {genres.map((g)=> (
+            <li key={g}>{g}</li>
+        
+        ))}
+      </ul>
+    </div>);
+}
+
+Movie.propTypes ={
+    id : PropTypes.number.isRequired,
+    coverImg : PropTypes.string.isRequired,
+    title : PropTypes.string.isRequired,
+    summary : PropTypes.string.isRequired,
+    genres : PropTypes.arrayOf(PropTypes.string).isRequired,
+
+};
+
+export default Movie;
